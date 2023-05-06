c272dbcc3408d2f4468057e4fff9ad1d071835e4
Ekaterina Egorova <61324163+EkaterinaEg@users.noreply.github.com>
2023-05-05 23:19:21 -0400

    feat: implement code for 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const { mkdir } = require('node:fs/promises');
+const fs = require('fs');
+const { join } = require('node:path');
+const path = require('path');
 
+async function makeDirectory() {
+  const folder = join(__dirname, 'files-copy');
+  const dirCreation = await mkdir(folder, {
+    recursive: true,
+  });
+
+  return dirCreation;
+}
+makeDirectory().catch(console.error);
+
+const folderFromCopyPath = '04-copy-directory/files';
+const fromDir = 'files';
+const toDir = 'files-copy';
+fs.readdir(folderFromCopyPath, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      fs.copyFile(
+        path.resolve(__dirname, fromDir, file),
+        path.resolve(__dirname, toDir, file),
+        (err) => {
+          if (err) throw err;
+          console.log('succesfully');
+        }
+      );
+    });
+  }
+});
