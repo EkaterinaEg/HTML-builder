df931c15add0c3e2d17c96c90086dfe0779d93ae
Ekaterina Egorova <61324163+EkaterinaEg@users.noreply.github.com>
2023-05-06 22:32:52 -0400

    feat: implement task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const process = require("node:process");
+const { stdin } = process;
+
+const folderPath = "05-merge-styles/styles";
+const dir = "styles";
+const dirForResultFile = "project-dist";
+const resultFileName = "project-dist/bundle.css";
+let writableStream = fs.createWriteStream(
+  path.resolve(__dirname, resultFileName)
+);
+// let readableStream = fs.createReadStream(path.resolve(__dirname, dir, file));
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      fs.stat(path.resolve(__dirname, dir, file), (err, fileStats) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (
+            fileStats.isFile() &&
+            path.extname(file).split(".")[1] === "css"
+          ) {
+            let readableStream = fs.createReadStream(
+              path.resolve(__dirname, dir, file)
+            );
+            readableStream.on("data", (data) => {
+              writableStream.write(data);
+            });
+          }
+        }
+      });
+    });
+  }
+});
